5a798f236d6d6c78bcd50714ed52113f3a8cf439
Mooaz Mohammed <madhmhmdsyd110@gmail.com>
2026-03-02 14:02:35 +0200

    adding changes to user model to fit local storage hive (toMap) and (fromMap)

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,13 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class UserModel {
   final String uid;
   final String email;
   final String? name;
   final String? photoUrl;
 
-  UserModel({
-    required this.uid,
-    required this.email,
-    this.name,
-    this.photoUrl,
-  });
+  UserModel({required this.uid, required this.email, this.name, this.photoUrl});
+
+  factory UserModel.fromFirebase(User firebaseUser) {
+    return UserModel(
+      uid: firebaseUser.uid,
+      email: firebaseUser.email ?? '',
+      photoUrl: firebaseUser.photoURL,
+      name: firebaseUser.displayName,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'uid': uid, 'email': email, 'name': name, 'photoUrl': photoUrl};
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'] ?? '', 
+      email: map['email'] ?? '',
+      name: map['name'] ?? '',
+      photoUrl: map['photoUrl'] ?? ''
+      );
+  }
 }
